520abb04c9e85d6e37ab775fc4f55325b89e896b
Csaba Andras <acsaba@gmail.com>
2022-02-20 21:30:29 +0100

    fix colors

--- hello/mandelbrot-quick.py
+++ hello/mandelbrot-quick.py
@@ -12,22 +12,29 @@ LCD = liblcd.LCD_3inch5()
 MAX_ITERATION = 100
 ABS_LIMIT = 100
 
+
+red = 0xf800
+green = 0x07e0
+blue = 0x001f
+
+def color(r,g,b):
+    ret = int((2**5)*r)
+    ret <<= 6
+    ret |= int((2**6)*g)
+    ret <<= 5
+    ret |= int((2**5)*b)
+    return ret
+
 rFreq = 3
 gFreq = 5
 bFreq = 8
-
 
 def calcColor(step):
     mandelColor = step / MAX_ITERATION
     r = math.sin(rFreq*mandelColor)/2+0.5
     g = math.sin(gFreq*mandelColor)/2+0.5
     b = math.sin(bFreq*mandelColor)/2+0.5
-    ret = int((2**5)*r)
-    ret <<= 5
-    ret |= int((2**5)*b)
-    ret <<= 6
-    ret |= int((2**6)*g)
-    return ret
+    return color(r,g,b)
 
 
 colors = []
@@ -39,7 +46,7 @@ def initColors():
         colors.append(calcColor(i))
 
 
-def color(step):
+def getColor(step):
     return colors[step]
 
 
@@ -57,15 +64,14 @@ def getPixelStep(x, y):
     return mandelbrot((x/480-0.5)*4.8, (y/320-0.5)*3.2)
 
 
-maxn = 10000
-buf = bytearray(maxn*2)
+maxBufP = 10000
+buf = bytearray(maxBufP*2)
 bufPos = 0
 
 
 def resetBuf():
     global bufPos
     bufPos = 0
-
 
 def writeColor(color):
     global bufPos
@@ -88,18 +94,17 @@ def repeatColor(color, n):
 
 
 def DrawRec(x1, x2, y1, y2, v11, v12, v21, v22):
-    # print('draw', x1, x2, y1, y2)
     global LCD
     if y1 == y2:
         resetBuf()
-        col = color(v11)
+        col = getColor(v11)
         writeColor(col)
         x = x1+1
         while x <= x2:
             if x == x2:
-                col = color(v21)
+                col = getColor(v21)
             else:
-                col = color(getPixelStep(x, y1))
+                col = getColor(getPixelStep(x, y1))
             writeColor(col)
             x += 1
         LCD.ShowBuffer(x1, x2, y1, y1, buf[:bufPos])
@@ -119,7 +124,7 @@ def DrawRec(x1, x2, y1, y2, v11, v12, v21, v22):
     v2o = getPixelStep(x2, yo)
 
     if v11 == v22 and v11 == v2o and v11 == v21 and v11 == vo2 and v11 == voo and v11 == vo1 and v11 == v12 and v11 == v1o:
-        col = color(v11)
+        col = getColor(v11)
         repeatColor(col, (x2-x1+1)*(y2-y1+1))
         LCD.ShowBuffer(x1, x2, y1, y2, buf[:bufPos])
         return
@@ -133,7 +138,6 @@ def DrawRec(x1, x2, y1, y2, v11, v12, v21, v22):
 WWW = 480
 HHH = 320
 
-
 def DrawAll():
     w = WWW-1
     h = HHH-1
@@ -146,9 +150,15 @@ def DrawSlow():
     for x in range(WWW):
         l = []
         for y in range(HHH):
-            col = color(getPixelStep(x, y))
+            col = getColor(getPixelStep(x, y))
             l = l + [col >> 8, col & 0xff]
         LCD.ShowBuffer(x, x, 0, 319, bytearray(l))
+
+
+
+class CoopDraw:
+    def __init__(self):
+        pass
 
 
 def main():
