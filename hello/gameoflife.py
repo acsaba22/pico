e40eaccd112760d22617155d5ea0f0f4e755cd8a
Csaba Andras <acsaba@gmail.com>
2022-02-22 20:49:59 +0100

    use liblcd

--- hello/gameoflife.py
+++ hello/gameoflife.py
@@ -1,193 +1,10 @@
 import liblcd
 import os
-
-
-import framebuf
 import time
-
-class Button(object):
-
-    def __init__(self, screen, box,
-                 text="",
-                 color_surface=liblcd.RGB_FB(128, 128, 128),
-                 color_surface_pressed=liblcd.RGB_FB(160, 160, 160),
-                 color_text=liblcd.RGB_FB(0, 0, 0)):
-        self._state = 0
-
-        self.screen = screen
-        self.box = liblcd.Box(int(box.x1), int(box.x2), int(box.y1), int(box.y2))
-
-        self.text = text
-        self.color_surface = color_surface
-        self.color_surface_pressed = color_surface_pressed
-        self.color_text = color_text
-        self.draw()
-
-    def draw(self):
-        if self._state:
-            self._drawPressed()
-        else:
-            self._drawNormal()
-
-    def setText(self, text):
-        self.text = text
-        self.draw()
-
-    def _getFB(self):
-        buf = bytearray(self.box.width * self.box.height * 2)
-        fb = framebuf.FrameBuffer(buf, self.box.width, self.box.height, framebuf.RGB565)
-        return fb, buf
-
-    def _drawEdge(self, fb):
-        pass
-
-    def _drawSurface(self, fb):
-        if self._state:
-            fb.fill_rect(0, 0, self.box.width, self.box.height, self.color_surface)
-        else:
-            fb.fill_rect(0, 0, self.box.width, self.box.height, self.color_surface_pressed)
-
-    def _drawNormal(self):
-        fb, buf = self._getFB()
-        self._drawSurface(fb)
-        self._drawEdge(fb)
-        if self.text:
-            fb.text(self.text, max(0, self.box.width//2 - len(self.text)*4), self.box.height // 2 - 4, self.color_text)
-        self.screen.ShowBufferAtBox(self.box, buf)
-
-    def _drawPressed(self):
-        fb, buf = self._getFB()
-        self._drawSurface(fb)
-        self._drawEdge(fb)
-        if self.text:
-            fb.text(self.text, max(0, self.box.width//2 - len(self.text)*4)+2, self.box.height // 2 - 2, self.color_text)
-        self.screen.ShowBufferAtBox(self.box, buf)
-
-    def _setState(self, new_state):
-        new_state = min(1, max(0, new_state))
-        if self._state == new_state:
-            return
-        self._state = new_state
-        self.draw()
-
-    def do(self, touch):
-        if touch != None:
-            c = liblcd.Coord(touch[0], touch[1])
-            if c in self.box:
-                self._setState(1)
-            elif self._state == 1:
-                # print ("left %s" % (c, ))
-                self._setState(0)
-        elif self._state == 1:
-            self._setState(0)
-            self.doPressed()
-            return True
-        return False
-
-    def doPressed(self):
-        pass
-
-class Button3D(Button):
-    def __init__(self, screen, box,
-                 color_edge_dark=liblcd.RGB_FB(64, 64, 64),
-                 color_edge_light=liblcd.RGB_FB(192, 192, 192),
-                 **kwargs):
-        self.color_edge_dark = color_edge_dark
-        self.color_edge_light = color_edge_light
-        Button.__init__(self, screen, box, kwargs)
-
-    def _drawUpLeftEdge(self, fb, color):
-        fb.hline(0, 0, self.box.width-1, color)
-        fb.hline(0, 1, self.box.width-3, color)
-        fb.vline(0, 2, self.box.height-3, color)
-        fb.vline(0, 3, self.box.height-5, color)
-
-    def _drawBottomRightEdge(self, fb, color):
-        fb.hline(1, self.box.height-2, self.box.width-2, color)
-        fb.hline(0, self.box.height-1, self.box.width-1, color)
-        fb.vline(self.box.width-2, 1, self.box.height-2, color)
-        fb.vline(self.box.width-1, 0, self.box.height-1, color)
-
-    def _drawEdge(self, fb):
-        if self._state:
-            self._drawBottomRightEdge(fb, self.color_edge_light)
-            self._drawUpLeftEdge(fb, self.color_edge_dark)
-        else:
-            self._drawBottomRightEdge(fb, self.color_edge_dark)
-            self._drawUpLeftEdge(fb, self.color_edge_light)
-
-class Counter(object):
-
-    def __init__(self, lcd):
-        self.sprite = liblcd.Sprite(lcd, 96, 8)
-        self.sprite.move(liblcd.Coord(10, 10))
-        self.fb = self.sprite.getFramebuffer()
-        self.reset()
-
-    def reset(self):
-        self.value = 0
-        self.update()
-        self.sprite.show()
-
-    def update(self):
-        self.fb.fill(liblcd.WHITE)
-        self.fb.text("counter: %d" % (self.value, ), 0, 0, liblcd.BLACK)
-        self.sprite.draw()
-
-    def increase(self, by=1):
-        self.value += by
-        self.update()
-
-    def hide(self):
-        self.sprite.hide()
-
-
-class Touch(object):
-    def __init__(self, screen):
-        self.screen = screen
-        self.reads = []
-        self._last_pos = None
-        self._read()
-
-    def do(self):
-        self._read()
-
-    def get(self):
-        return self._last_pos
-
-    def _read(self):
-        def dist(p1, p2):
-            return abs(p1[0]-p2[0])+abs(p1[1]-p2[1])
-        new_time, new_pos = time.ticks_ms(), self.screen.TouchGet()
-        new_value = new_time, new_pos
-        has_value = 0
-        self.reads = [v for v in self.reads if time.ticks_diff(new_time, v[0]) < 200 and v[1] is not None]
-        if len(self.reads) < 3:
-            self._last_pos = None
-        elif new_pos:
-            c = 0
-            for v in self.reads:
-                if v[1] and dist(v[1], new_pos) < 20:
-                    c += 1
-            if c > len(self.reads)//3:
-                self._last_pos = new_pos
-        if new_pos:
-            self.reads.append(new_value)
-
-
-
-
-
-
-
-
-
-
-
 
 
 LCD = liblcd.LCD_3inch5()
-smartTouch = Touch(LCD)
+smartTouch = liblcd.SmartTouch(LCD)
 
 BLACK_1_BYTE = 0x00
 WHITE_1_BYTE = 0xff
@@ -209,9 +26,6 @@ def Fill(x1, x2, y1, y2, color1Byte):
     LCD.ShowBuffer(x1, x2, y1, y2, b)
 
 
-
-
-
 H = 320
 MAXP = 10000
 MinEditZoom = 4
@@ -226,11 +40,11 @@ class Controller:
         Fill(320, 480, 160, 160, BLACK_1_BYTE)
         Fill(320, 480, 240, 240, BLACK_1_BYTE)
 
-        self.bMinus = Button(LCD, liblcd.Box(322, 399, 241, 319), text= "---")
-        self.bPlus = Button(LCD, liblcd.Box(401, 479, 241, 319), text= "+++")
+        self.bMinus = liblcd.Button(LCD, liblcd.Box(322, 399, 241, 319), text= "---")
+        self.bPlus = liblcd.Button(LCD, liblcd.Box(401, 479, 241, 319), text= "+++")
 
-        self.bLoad = Button(LCD, liblcd.Box(322, 399, 161, 239), text= "LOAD")
-        self.bSave = Button(LCD, liblcd.Box(401, 479, 161, 239), text= "SAVE")
+        self.bLoad = liblcd.Button(LCD, liblcd.Box(322, 399, 161, 239), text= "LOAD")
+        self.bSave = liblcd.Button(LCD, liblcd.Box(401, 479, 161, 239), text= "SAVE")
 
 
         self.alive = set()
@@ -247,6 +61,7 @@ class Controller:
         self.firstTouch = None
 
         self.moving = False
+
     # def refreshBoard(self):
     #     clearGameBoard()
     #     self.DrawBoard()
@@ -268,9 +83,7 @@ class Controller:
                 k = i*self.width
                 Fill(0, H-1, k, k, 0)
                 Fill(k, k, 0, H-1, 0)
-        for c in self.alive:
-            smartTouch.do()
-            self.fillCell(c, 0x00)
+        self.showCells()
 
     def hideCells(self):
         for c in self.alive:
