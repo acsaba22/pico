65e9fc87ba620c5516308b0260be4a210d9ceee3
Csaba Andras <acsaba@gmail.com>
2026-02-17 00:29:13 +0100

    Add constant for lednum

--- flums2026/neopixel1.py
+++ flums2026/neopixel1.py
@@ -1,12 +1,14 @@
 import neopixel
 import machine
 
+LEDNUM = 32
+
 p = machine.Pin(0, machine.Pin.OUT)
-n = neopixel.NeoPixel(p, 32)
+n = neopixel.NeoPixel(p, LEDNUM)
 
 
 # Draw a red gradient.
-for i in range(32):
+for i in range(LEDNUM):
     n[i] = (i, 0, 0)
 
 # Update the strip.
